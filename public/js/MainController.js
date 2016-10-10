76f445db764fe9a37bb32e6b97f84b43dac6ed77
Justin <JustinJonsson@users.noreply.github.com>
2016-10-09 21:48:11 -0400

    Complete rewrite

---
+++ public/js/MainController.js
@@ -0,0 +1,20 @@
+function MainController( userService ) {
+  var self = this;
+
+  self.selected     = null;
+  self.users        = [ ];
+  self.selectUser   = selectUser;
+
+  // Load all registered users
+
+  userService
+    .loadAllUsers()
+    .then( function( users ) {
+      self.users    = [].concat(users);
+      self.selected = users[0];
+    });
+
+  function selectUser ( user ) {
+    self.selected =  user;
+  }
+}
