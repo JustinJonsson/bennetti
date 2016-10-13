619e624ecb88ba9488e769e05dcfa9068f6ad764
JustinJonsson <junk.jonsson@gmail.com>
2016-10-12 21:18:26 -0400

    adding checkboxes. still WIP.

--- public/js/UserFactory.js
+++ public/js/UserFactory.js
@@ -4,18 +4,22 @@ function UserFactory(){
   Users.userlist = [
     {
       name: 'Justin Jonsson',
+      login: 'malapardis',
       content: 'Growler'
     },
     {
       name: 'Clayton Swindoll',
+      login: 'autolyze',
       content: 'GoGo Grill'
     },
     {
       name: 'Raphael Kumar',
+      login: 'webswinger',
       content: 'Tajin'
     },
     {
       name: 'Katie Shields',
+      login: 'dibdob',
       content: 'Chopt'
     }
   ];
